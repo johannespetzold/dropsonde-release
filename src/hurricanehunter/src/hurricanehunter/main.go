a337b41e0b2d96fe22fb4b7a2d5d394ecbf98e95
Johannes Tuchscherer <jtuchscherer@pivotallabs.com>
2014-04-01 17:07:17 -0600

    [#67664082] Bump dropsonde version.
    Move dropsonde example application to top level.
    
    Signed-off-by: caleb miles <cmiles@pivotallabs.com>

---
+++ src/hurricanehunter/src/hurricanehunter/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/cloudfoundry-incubator/dropsonde"
+	"hurricanehunter/hunter"
+	"log"
+	"net/http"
+)
+
+func main() {
+	log.Print("Launching HurricaneHunter on port 8080 … testing dropsondes")
+
+	handler := new(hunter.Handler)
+	instrumentedHunter := dropsonde.InstrumentedHandler(handler, "hunter", 0)
+	log.Fatal(http.ListenAndServe(":8080", instrumentedHunter))
+}
